4efcfe1eb0edcd46a0062b24bdc907556e920d4c
Icarz <Mr.rghay@gmail.com>
2024-09-15 05:40:56 -0700

    task_13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the head of the list
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+    /* Mark the parameter as unused */
+    (void) head;
+
+    return (0);
+}
