7c911214514f39fed62b81cf16cb5ccd9195d740
icarz <mr.rghay@gmail.com>
2025-01-13 05:12:50 -0800

    review

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
+# Puppet script to create ssh config file
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',    # Path to the SSH client configuration file
-  line  => 'PasswordAuthentication no',  # Disable password authentication
-  match => '^#PasswordAuthentication',  # Ensure the correct line is updated
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',      # Path to the SSH client configuration file
-  line  => 'IdentityFile ~/.ssh/school',  # Specify the private key to use
-  match => '^#IdentityFile',            # Ensure the correct line is updated
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
