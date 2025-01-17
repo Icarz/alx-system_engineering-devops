21406ad8b9be8580120b986af383e7fe3737475a
icarz <mr.rghay@gmail.com>
2025-01-16 16:46:57 -0800

    task_1000

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This script sends a request to the URL passed as an argument and displays only the status code
 
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -o /dev/null -sw "%{http_code}" $1
 
