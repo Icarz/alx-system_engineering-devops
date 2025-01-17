c3fb43277f9a14ec0319010ec08d638a249b9425
icarz <mr.rghay@gmail.com>
2025-01-17 15:09:38 -0800

    whitespace

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,16 +5,15 @@ def find_peak(list_of_integers):
     """Find a peak element in the list of integers"""
     if not list_of_integers:
         return None
-    
+
     left, right = 0, len(list_of_integers) - 1
-    
+
     while left <= right:
         mid = (left + right) // 2
-        
+
         if (mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]) and (mid == len(list_of_integers) - 1 or list_of_integers[mid + 1] <= list_of_integers[mid]):
             return list_of_integers[mid]
         elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
             right = mid - 1
         else:
             left = mid + 1
-
